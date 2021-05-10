27598993f09001037bb584262c8b54ecda4a2bbc
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-10 10:56:21 +0200

    verify admin middleware

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -1,1 +1,27 @@
-module.exports = (req, res, next) => next()
+const db = require('firebase-admin').firestore()
+
+module.exports = async (req, res, next) => {
+    let idToken
+    if (req.headers.authorization == null || !req.headers.authorization.startsWith('Bearer ')) {
+        console.log('Unrecognized token format')
+        return res.status(400).json({ error: 'Unauthorized operation' })
+    }
+    else {
+        idToken = req.headers.authorization.split('Bearer ')[1]
+    }
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken)
+        req.user = decodedToken
+
+        const adminQuery = await db.collection('admins').where('uid', '==', decodedToken.uid).get()
+        if (adminQuery.docs.length === 0) {
+            return res.status(403).json({ error: 'Unauthorized' })
+        }
+
+        return next()
+    }
+    catch (error) {
+        console.error(error)
+        return res.status(403).json(error)
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const Router = require('express').Router
 
+const routes = Router()
+
 const auth = require('./auth')
 const profile = require('./profile')
-
-const routes = Router()
+const admin = require('./admin')
 
 routes.use('/auth', auth)
 routes.use('/profile', profile)
+routes.use('/admin', admin)
 
 module.exports = routes
