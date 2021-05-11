cb3fba27b117e967272f3b12ae0cc9d2e0c300ee
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-11 17:47:52 +0200

    initialize firebase

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require('express')
 
 const admin = require('firebase-admin')
 admin.initializeApp()
+const firebase = require('firebase')
+firebase.initializeApp({
+    apiKey: "AIzaSyCxzqaOCg6HNjeMVcjO8aJq3pwb6ZkiWmw",
+    authDomain: "sahm-b2b16.firebaseapp.com",
+    projectId: "sahm-b2b16",
+    storageBucket: "sahm-b2b16.appspot.com",
+    messagingSenderId: "888002118145",
+    appId: "1:888002118145:web:c8b98a026a40b0daad3600",
+    measurementId: "G-54QZXQ8HLN"
+})
 
 
 const app = express()
