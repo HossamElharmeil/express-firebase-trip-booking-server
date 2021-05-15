f186068671372f315379072b14cc947a3724e444
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-15 18:04:27 +0200

    fix: remove error logging for auth

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -15,7 +15,6 @@ const verifyToken = async (req, res, next) => {
         return next()
     }
     catch (error) {
-        console.error(error)
         return res.status(403).json(error)
     }
 }
