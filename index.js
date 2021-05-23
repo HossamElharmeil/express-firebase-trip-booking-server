3bcc2d091e57314989fdd772aaaa7bce2c6b62ee
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-23 15:56:26 +0200

    index: rearrangement

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const PORT = process.env.PORT || 8080
 
-const express = require('express')
-
-const admin = require('firebase-admin')
-admin.initializeApp()
-
-const firebase = require('firebase')
+require('firebase-admin').initializeApp()
+const busboy = require('connect-busboy')
 const config = require('./util/config').firebaseConfig
-firebase.initializeApp(config)
+const express = require('express')
+require('firebase').initializeApp(config)
+const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
-
-const busboy = require('connect-busboy')
 app.use(busboy())
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
-
-const routes = require('./routes')
 app.use(routes)
-
 app.get('/', (req, res) => {
     res.send('Mmyallo!')
 })
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
