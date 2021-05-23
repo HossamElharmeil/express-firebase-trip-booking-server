3a14b8839e535aefc009f77fed350b823bd32870
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-23 18:43:18 +0200

    images: upload profile image

--- routes/profile/imageRouter.js
+++ routes/profile/imageRouter.js
@@ -7,6 +7,7 @@ const path = require('path')
 const verifyToken = require('../../middleware/verifyToken')
 
 const db = require('firebase-admin').firestore()
+const auth = require('firebase-admin').auth()
 const storage = require('firebase-admin').storage()
 
 const imageRouter = Router()
@@ -62,6 +63,52 @@ imageRouter.post('/uploadImage', async (req, res) => {
             const images = imageQuery? imageQuery.images ? imageQuery.images : [] : []
             images.concat([imageUrl])
             await db.collection('images').doc(uid).update({ images })
+
+            return res.json({ message: 'Image uploaded successfully', imageUrl })
+        }
+        catch (error) {
+            console.error(error)
+            return res.status(500).json({ error: 'Something went wrong' })
+        }
+    })
+    
+    req.pipe(busboy)
+})
+
+imageRouter.post('/uploadProfile', (req, res) => {
+    const uid = req.user.uid
+    
+    const busboy = new BusBoy({ headers: req.headers })
+    let imageFileName
+    let imageToBeUploaded = {}
+
+    busboy.on('file',  (fieldname, file, filename, encoding, mimetype) => {
+        if (mimetype !== 'image/png' && mimetype !== 'image/jpeg' && mimetype !== 'image/jpg') 
+            return res.status(400).json({ error: 'Unsupported file format' })
+        
+        const imageExtension = path.extname(filename)
+        imageFileName = `${Math.round(Math.random() * 100000000000)}${imageExtension}`
+        const filepath = path.join(os.tmpdir(), imageFileName)
+
+        imageToBeUploaded = { filepath, mimetype }
+
+        file.pipe(fs.createWriteStream(filepath))
+    });
+
+    busboy.on('finish', async () => {
+        try {
+            await storage.bucket(config.storageBucket).upload(imageToBeUploaded.filepath, {
+                resumable: false,
+                metadata: {
+                    metadata: {
+                        contentType: imageToBeUploaded.mimetype
+                    }
+                }
+            })
+            const imageUrl =
+                `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
+            
+            auth.updateUser(uid, { imageUrl })
 
             return res.json({ message: 'Image uploaded successfully', imageUrl })
         }
