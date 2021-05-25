900978a9c1b5878f833bd9122102421635c01057
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-25 13:35:14 +0200

    withdraw

--- routes/wallet/userRouter.js
+++ routes/wallet/userRouter.js
@@ -19,4 +19,31 @@ userRouter.get('/getWallet', async (req, res) => {
     }
 })
 
+userRouter.post('/withdraw', async (req, res) => {
+    const uid = req.user.uid
+    const amount = req.body.amount
+    const account = req.body.account
+
+    try {
+        const wallet = (await db.collection('wallets').doc(uid).get()).data()
+        
+        const newAmount = wallet.amount - amount
+        await db.collection('wallets').doc(uid).set({ amount: newAmount })
+        
+        const transaction = {
+            uid,
+            amount,
+            account,
+            type: 'withdraw'
+        }
+        await db.collection('transactions').add(transaction)
+
+        return res.json({ success: 'Transaction done successfully' })
+    }
+    catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: 'Something went wrong' })
+    }
+})
+
 module.exports = userRouter
