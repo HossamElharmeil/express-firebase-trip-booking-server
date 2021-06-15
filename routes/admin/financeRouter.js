52308a99ac9d67cc1762cb707c2dae9a4941fbdf
HossamElharmeil <hossam.elharmil@gmail.com>
2021-06-15 14:39:39 +0200

    get finance

--- routes/admin/financeRouter.js
+++ routes/admin/financeRouter.js
@@ -34,4 +34,22 @@ financeRouter.get('/getDebt', async (_, res) => {
     }
 })
 
+financeRouter.get('/getFinance', async (_, res) => {
+    try {
+        const payments = (await db.collection('payments').get()).docs.map(doc => doc.data())
+        
+        let debt = 0
+        const walletQuery = await db.collection('wallets').get()
+        walletQuery.docs.forEach(doc => {
+            debt += doc.data().amount ?? 0
+        })
+    
+        return res.json({ payments, debt })
+    }
+    catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: 'Something went wrong' })
+    }
+})
+
 module.exports = financeRouter
