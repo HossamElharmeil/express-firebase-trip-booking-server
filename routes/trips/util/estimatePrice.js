ed80bea8c024c051ef12e30f2444fe34ffeb760d
HossamElharmeil <hossam.elharmil@gmail.com>
2021-06-24 14:40:12 +0200

    round estimate price

--- routes/trips/util/estimatePrice.js
+++ routes/trips/util/estimatePrice.js
@@ -4,7 +4,7 @@ const estimatePrice = (pickup, dropoff) => {
     const latDiff = Math.abs(pickup.lat - dropoff.lat)
     const lngDiff = Math.abs(pickup.lng - dropoff.lng)
 
-    return latDiff + lngDiff * 100
+    return Math.round(latDiff + lngDiff * 100)
 }
 
 module.exports = estimatePrice
